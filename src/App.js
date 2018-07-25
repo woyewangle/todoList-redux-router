0b7db73e8628549df189c7a1c233cb7ff04c60ee
LEON YU (ITA-ISDC-ISD-OOCLL/ZHA) <YULE4@OOCL.COM>
2018-07-25 19:43:20 +0800

    在远程调用API的基础上增加router

--- src/App.js
+++ src/App.js
@@ -3,30 +3,34 @@ import './App.css';
 import Header from "./container/headerContainer";
 import Footer from "./container/FooterContainer";
 import TodoList from "./container/TodoListContainer";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
     super(props);
   }
 
-
-
   render() {
-
     return (
-      <div className="container">
-        <div>
-          <h2>Jquery To Do List</h2>http://localhost:8080/api/todos
-          <p>
-            <em>Simple Todo List with adding and filter by diff status.</em>
-          </p>
+      <Router>
+        <div className="container">
+          <div>
+            <h2>Jquery To Do List</h2>http://localhost:8080/api/todos
+            <p>
+              <em>Simple Todo List with adding and filter by diff status.</em>
+            </p>
+          </div>
+          <Header/>
+          {/*<TodoList/>*/}
+          <Route exact path="/" component={Items} />
+          <Route path="/active" component={Items} />
+          <Route path="/complete" component={Items} />
+          <Footer/>
+
+
         </div>
-        <Header/>
-       <TodoList/>
-        <Footer/>
+      </Router>
 
-
-      </div>
     );
   }
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,46 +17,67 @@ export default class header extends Component {
 
   render(){
     return(
+      {/*<div>*/}
+        {/*<ul className="filters">*/}
+          {/*<li>*/}
+            {/*<a*/}
+              {/*href="#all"*/}
+              {/*onClick={e => this.showFilterList(e)}*/}
+              {/*data-filter="all"*/}
+              {/*className={classNames({*/}
+                {/*selected: this.props.statusOfList === Todo.ALL*/}
+              {/*})}*/}
+            {/*>*/}
+              {/*ALL*/}
+            {/*</a>*/}
+          {/*</li>*/}
+          {/*<li>*/}
+            {/*<a*/}
+              {/*href="#active"*/}
+              {/*onClick={e => this.showFilterList(e)}*/}
+              {/*data-filter="active"*/}
+              {/*className={classNames({*/}
+                {/*selected: this.props.statusOfList === Todo.ACTIVE*/}
+              {/*})}*/}
+            {/*>*/}
+              {/*Active*/}
+            {/*</a>*/}
+          {/*</li>*/}
+          {/*<li>*/}
+            {/*<a*/}
+              {/*href="#completed"*/}
+              {/*onClick={e => this.showFilterList(e)}*/}
+              {/*data-filter="completed"*/}
+              {/*className={classNames({*/}
+                {/*selected: this.props.statusOfList === Todo.COMPLETED*/}
+              {/*})}*/}
+            {/*>*/}
+              {/*Complete*/}
+            {/*</a>*/}
+          {/*</li>*/}
+        {/*</ul>*/}
+      {/*</div>*/}
+    <Router>
       <div>
-        <ul className="filters">
+        <ul id="filters">
           <li>
-            <a
-              href="#all"
-              onClick={e => this.showFilterList(e)}
-              data-filter="all"
-              className={classNames({
-                selected: this.props.statusOfList === Todo.ALL
-              })}
-            >
-              ALL
-            </a>
+            <Link to='/'> <a href="#" data-filter="all" className={status == " all" ? " selected" : ""}
+                             onClick={e => this.showFilterList(e)}>All</a></Link>
           </li>
           <li>
-            <a
-              href="#active"
-              onClick={e => this.showFilterList(e)}
-              data-filter="active"
-              className={classNames({
-                selected: this.props.statusOfList === Todo.ACTIVE
-              })}
-            >
-              Active
-            </a>
+            <Link to='/active'> <a href="#" data-filter="active"
+                                   className={status == " active" ? "selected" : ""}
+                                   onClick={e => this.showFilterList(e)}>Active</a></Link>
           </li>
           <li>
-            <a
-              href="#completed"
-              onClick={e => this.showFilterList(e)}
-              data-filter="completed"
-              className={classNames({
-                selected: this.props.statusOfList === Todo.COMPLETED
-              })}
-            >
-              Complete
-            </a>
+            <Link to='/complete'> <a href="#" data-filter="complete"
+                                     className={status == " complete" ? "selected" : ""}
+                                     onClick={e => this.showFilterList(e)}>Complete</a></Link>
           </li>
         </ul>
       </div>
+
+    </Router>
     );
   }
 }
